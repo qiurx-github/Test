ac0c233b7b8c4a86b63b08f895315d3651be31f6
rx qiu <513587163@qq.com>
2017-03-02 17:44:44 +0800

    添加项目文件。

---
+++ NetworkExamination.Models/ExamPaper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkExamination.Models
+{
+    public class ExamPaper
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public decimal Score { get; set; }
+        public Dictionary<int, char> AnswerCard { get; set; }
+        public string Content { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
